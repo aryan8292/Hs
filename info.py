91ba6b6f4ac574a730dfa07c6a968e7d285b54a1
ary965874 <lillijorres59@gmail.com>
2025-06-11 01:47:56 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'media_search')
 API_ID = int(environ.get('API_ID', '26513278'))
 API_HASH = environ.get('API_HASH', '7a3df75c8ed3520737e343e9f049166b')
-BOT_TOKEN = environ.get('BOT_TOKEN', "6381892800:AAFOhLNpCIComIuyjgW1O2rzQD_ACXVlWXk")
+BOT_TOKEN = environ.get('BOT_TOKEN', "7610200234:AAE9OqwCpLNHoC1G3Wh2YSSUnSzwNRBgVB0")
 
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
