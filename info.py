8ad203e61ecc9cbe2e09a5e619273fe685e8adef
ary965874 <lillijorres59@gmail.com>
2025-06-11 03:08:24 +0530

    Update info.py

--- info.py
+++ info.py
@@ -40,13 +40,13 @@ REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_
 support_chat_id = environ.get('SUPPORT_CHAT_ID', '-1002682692512') #Support Chat Id
 SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(support_chat_id) else None
 
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://mojahidalam8092:Boss.1234@cluster0.qecvnc7.mongodb.net/?retryWrites=true&w=majority") #MongoDB Url
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://unknown4u1082:unknown4u1082@cluster0.oq1fuwz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") #MongoDB Url
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'mojahidalam8092')
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'moviesfile')
 
 # If MULTIPLE_DB Is True Then Fill DATABASE_URI2 Value Else You Will Get Error.
-MULTIPLE_DB = is_enabled(os.environ.get('MULTIPLE_DB', "True"), True) # Type True For Turn On MULTIPLE DB FUNTION 
-DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://mojahidalam8092:KV7D2VuonCcWlcA0@cluster0.mhmnwab.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MULTIPLE_DB = is_enabled(os.environ.get('MULTIPLE_DB', "False"), False) # Type True For Turn On MULTIPLE DB FUNTION 
+DATABASE_URI2 = environ.get('DATABASE_URI2', "")
 
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/streamtub')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/streamtub')
